76723cf196e227ed726c2b53230f71bcfb40e86f
David Huggins-Daines <dhd@pobox.com>
2001-04-16 18:15:13 +0000

    Import of DJHD/Audio-OSS-0.0501 from CPAN.
    gitpan-cpan-distribution: Audio-OSS
    gitpan-cpan-version:      0.0501
    gitpan-cpan-path:         DJHD/Audio-OSS-0.0501.tar.gz
    gitpan-cpan-author:       DJHD
    gitpan-cpan-maturity:     released

--- OSS.pm
+++ OSS.pm
@@ -12,119 +12,82 @@
 package Audio::OSS;
 use strict;
 
-# Defines constants, which is why it needs to be done in BEGIN{}
-# Also creates @EXPORT_OK, hence the push below
-BEGIN {
-    require Audio::OSS::Constants;
-}
-
-# We should generate these in Makefile.PL, but laziness says they're
-# likely to stay the same for binary compatibility purposes...
+# We should maybe generate these in Makefile.PL, but they don't use
+# any types that are likely to change alignment or size between
+# platforms.
 use constant CINFO_TMPL => 'lll';
 use constant BINFO_TMPL => 'llll';
+use constant MINFO_TMPL => 'a16 a32 l';
 
-use vars qw($VERSION @ISA @EXPORT_OK %EXPORT_TAGS);
+use vars qw($VERSION @ISA @EXPORT_OK %EXPORT_TAGS @DevNames);
 require Exporter;
 @ISA = qw(Exporter);
-push @EXPORT_OK, qw(dsp_sync dsp_reset dsp_get_caps
-		    set_sps set_fmt set_fragment
-		    get_fmt get_supported_fmts
+
+BEGIN {
+    # Defines constants, which is why it needs to be done in BEGIN{}
+    # Also creates @EXPORT_OK, hence the push below
+    require Audio::OSS::Constants;
+
+    # Special-case a few useful constants
+    unless (defined(&AFMT_S16_NE)) {
+	if (unpack("L", pack("C*", 1, 2, 3, 4)) == 0x01020304) {
+	    *AFMT_S16_NE = sub () { AFMT_S16_BE() };
+	} else {
+	    *AFMT_S16_NE = sub () { AFMT_S16_LE() };
+	}
+    }
+    # Pseudo-ioctls (we assume that various OSes will form these the
+    # same way...)
+    *SOUND_MIXER_READ = sub () { SOUND_MIXER_READ_VOLUME() - SOUND_MIXER_VOLUME() };
+    *SOUND_MIXER_WRITE = sub () { SOUND_MIXER_WRITE_VOLUME() - SOUND_MIXER_VOLUME() };
+    push @EXPORT_OK, qw(SOUND_MIXER_READ SOUND_MIXER_WRITE);
+    push @{$EXPORT_TAGS{mixer}}, qw(SOUND_MIXER_READ SOUND_MIXER_WRITE);
+}
+
+# Don't bother getting this from the header file
+@DevNames = qw(
+	       vol bass treble synth pcm speaker line
+	       mic cd mix pcm2 rec igain ogain line1 line2
+	       line3 dig1 dig2 dig3 phin phout video radio monitor
+	      );
+
+# Use push, because BEGIN blocks may frob these
+push @EXPORT_OK, qw(dsp_sync dsp_reset set_fragment get_fmt
 		    get_outbuf_ptr get_inbuf_ptr
-		    get_outbuf_info get_inbuf_info);
-%EXPORT_TAGS = (
-		funcs => [qw[
-			     dsp_sync
-			     dsp_reset
-			     dsp_get_caps
-			     set_sps
-			     set_fmt
-			     get_supported_fmts
-			     set_fragment
-			     get_outbuf_ptr
-			     get_inbuf_ptr
-			     get_outbuf_info
-			     get_inbuf_info
-			    ]],
-		formats => [qw[
-			       AFMT_QUERY
-			       AFMT_S16_NE
-			       AFMT_S16_LE
-			       AFMT_S16_BE
-			       AFMT_U16_LE
-			       AFMT_U16_BE
-			       AFMT_U8
-			       AFMT_MU_LAW
-			       AFMT_A_LAW
-			      ]],
-		caps => [qw[
-			    DSP_CAP_REVISION
-			    DSP_CAP_DUPLEX
-			    DSP_CAP_REALTIME
-			    DSP_CAP_BATCH
-			    DSP_CAP_COPROC
-			    DSP_CAP_TRIGGER
-			    DSP_CAP_MMAP
-			    DSP_CAP_MULTI
-			    DSP_CAP_BIND
-			   ]],
-	       );
+		    get_outbuf_info get_inbuf_info
+		    mixer_read mixer_write @DevNames);
+push @{$EXPORT_TAGS{funcs}},
+    qw[
+       dsp_sync
+       dsp_reset
+       set_fragment
+       get_fmt
+       get_outbuf_ptr
+       get_inbuf_ptr
+       get_outbuf_info
+       get_inbuf_info
+       mixer_read
+       mixer_write
+      ];
 
-# We may have to 'filter' out constants that don't actually exist in
-# @CONSTANTS here...  although the ones above should all be common
-
-$VERSION=0.05;
+$VERSION=0.05_01;
 
 sub dsp_reset {
     my $dsp = shift;
-
     ioctl $dsp, SNDCTL_DSP_SYNC, 0 or return undef;
     ioctl $dsp, SNDCTL_DSP_RESET, 0;
 }
 
 sub dsp_sync {
     my $dsp = shift;
-
     ioctl $dsp, SNDCTL_DSP_SYNC, 0;
-}
-
-sub dsp_get_caps {
-    my $dsp = shift;
-    my $caps = pack "L";
-    ioctl $dsp, SNDCTL_DSP_GETCAPS, $caps or return undef;
-
-    return $caps;
-}
-
-sub set_sps {
-    my ($dsp, $sps) = @_;
-
-    my $spd = pack "L", $sps;
-    ioctl $dsp, SNDCTL_DSP_SPEED, $spd or return undef;
-    return unpack "L", $spd;
-}
-
-sub set_fmt {
-    my ($dsp, $fmt) = @_;
-
-    my $sfmt = pack "L", $fmt;
-    ioctl $dsp, SNDCTL_DSP_SETFMT, $sfmt or return undef;
-    return unpack "L", $sfmt;
 }
 
 sub get_fmt {
     my $dsp = shift;
-
     my $sfmt = pack "L", AFMT_QUERY;
     ioctl $dsp, SNDCTL_DSP_SETFMT, $sfmt or return undef;
     return unpack "L", $sfmt;
-}
-
-sub get_supported_fmts {
-    my $dsp = shift;
-
-    my $fmts = pack "L";
-    ioctl $dsp, SNDCTL_DSP_GETFMTS, $fmts or return undef;
-    return unpack "L", $fmts;
 }
 
 sub set_fragment {
@@ -168,6 +131,61 @@ sub get_inbuf_info {
     return unpack BINFO_TMPL, $binfo;
 }
 
+# Some constants may not be defined, don't define their subs either
+BEGIN {
+    if (defined &SOUND_MIXER_INFO) {
+	*get_mixer_info = sub {
+	    my $mixer = shift;
+	    my $minfo = pack MINFO_TMPL;
+	    ioctl($mixer, SOUND_MIXER_INFO(), $minfo) or return undef;
+	    return unpack MINFO_TMPL, $minfo;
+	};
+	push @EXPORT_OK, 'get_mixer_info';
+	push @{$EXPORT_TAGS{funcs}}, 'get_mixer_info';
+    }
+}
+
+# Templated ioctls that just read or write a single integer value
+BEGIN {
+    no strict 'refs';
+    my @rw_ioctls = (
+		     dsp_get_caps => SNDCTL_DSP_GETCAPS,
+		     get_supported_fmts => SNDCTL_DSP_GETFMTS,
+		     set_sps => SNDCTL_DSP_SPEED,
+		     set_fmt => SNDCTL_DSP_SETFMT,
+		     set_stereo => SNDCTL_DSP_STEREO,
+		     mixer_read_devmask => SOUND_MIXER_READ_DEVMASK,
+		     mixer_read_recmask => SOUND_MIXER_READ_RECMASK,
+		     mixer_read_stereodevs => SOUND_MIXER_READ_STEREODEVS,
+		     mixer_read_caps => SOUND_MIXER_READ_CAPS,
+		    );
+    while (my ($sub, $ioctl) = splice @rw_ioctls, 0, 2) {
+	*$sub = sub {
+	    my $fh = shift;
+	    my $in = shift || 0;
+	    my $out = pack "L", $in;
+	    ioctl($fh, $ioctl, $out) or return undef;
+	    return unpack "L", $out;
+	};
+	push @EXPORT_OK, $sub;
+	push @{$EXPORT_TAGS{funcs}}, $sub;
+    }
+}
+
+sub mixer_read {
+    my ($mixer, $channel) = @_;
+    my $vol = pack "L";
+    ioctl($mixer, SOUND_MIXER_READ + $channel, $vol) or return undef;
+    return unpack "L", $vol;
+}
+
+sub mixer_write {
+    my ($mixer, $channel, $left, $right) = @_;
+    my $vol = pack("L", $left | ($right << 8));
+    ioctl($mixer, SOUND_MIXER_WRITE + $channel, $vol) or return undef;
+    return unpack "L", $vol;
+}
+
 1;
 __END__
 
@@ -177,7 +195,7 @@ Audio::OSS - pure-perl interface to OSS (open sound system) audio devices
 
 =head1 SYNOPSIS
 
-  use Audio::OSS qw(:funcs :formats);
+  use Audio::OSS qw(:funcs :formats :mixer);
 
   my $dsp = IO::Handle->new("</dev/dsp") or die "open failed: $!";
   dsp_reset($dsp) or die "reset failed: $!";
@@ -194,6 +212,9 @@ Audio::OSS - pure-perl interface to OSS (open sound system) audio devices
   my ($frags_avail, $frags_total, $fragsize, $bytes_avail)
       = get_outbuf_info($dsp);
   my ($bytes, $blocks, $dma_ptr) = get_outbuf_ptr($dsp);
+
+  my $mixer = IO::Handle->new("</dev/mixer") or die "open failed: $!";
+  my $miclevel = mixer_read($mixer, SOUND_MIXER_MIC);
 
 =head1 DESCRIPTION
 
@@ -231,12 +252,24 @@ operations on the PCM audio device:
   dsp_get_caps
   set_sps
   set_fmt
+  set_stereo
   get_supported_fmts
   set_fragment
   get_outbuf_ptr
   get_inbuf_ptr
   get_outbuf_info
   get_inbuf_info
+  mixer_read_devmask
+  mixer_read_recmask
+  mixer_read_stereodevs
+  mixer_read_caps
+  mixer_read
+  mixer_write
+
+Some functions are exported only if the underlying support for them
+exists on your operating system, namely:
+
+  get_mixer_info
 
 =item C<:formats>
 
@@ -268,6 +301,50 @@ the return value from C<dsp_get_caps>:
   DSP_CAP_MMAP
   DSP_CAP_MULTI
   DSP_CAP_BIND
+
+=item C<:mixer>
+
+This tag imports the following constants, which are used in mixer
+operations:
+
+  SOUND_MIXER_NRDEVICES
+  SOUND_MIXER_VOLUME
+  SOUND_MIXER_BASS
+  SOUND_MIXER_TREBLE
+  SOUND_MIXER_SYNTH
+  SOUND_MIXER_PCM
+  SOUND_MIXER_SPEAKER
+  SOUND_MIXER_LINE
+  SOUND_MIXER_MIC
+  SOUND_MIXER_CD
+  SOUND_MIXER_IMIX
+  SOUND_MIXER_ALTPCM
+  SOUND_MIXER_RECLEV
+  SOUND_MIXER_IGAIN
+  SOUND_MIXER_OGAIN
+  SOUND_MIXER_LINE1
+  SOUND_MIXER_LINE2
+  SOUND_MIXER_LINE3
+  SOUND_MIXER_DIGITAL1
+  SOUND_MIXER_DIGITAL2
+  SOUND_MIXER_DIGITAL3
+  SOUND_MIXER_PHONEIN
+  SOUND_MIXER_PHONEOUT
+  SOUND_MIXER_VIDEO
+  SOUND_MIXER_RADIO
+  SOUND_MIXER_MONITOR
+  SOUND_MIXER_NONE
+  SOUND_ONOFF_MIN
+  SOUND_ONOFF_MAX
+
+  SOUND_MIXER_RECSRC
+  SOUND_MIXER_DEVMASK
+  SOUND_MIXER_RECMASK
+  SOUND_MIXER_CAPS
+  SOUND_CAP_EXCL_INPUT
+  SOUND_MIXER_STEREODEVS
+  SOUND_MIXER_OUTSRC
+  SOUND_MIXER_OUTMASK
 
 =back
 
@@ -313,6 +390,63 @@ F<E<lt>sys/soundcard.hE<gt>> or F<E<lt>linux/soundcard.hE<gt>>.
   SOUND_PCM_READ_BITS
   SOUND_PCM_READ_FILTER
 
+  SOUND_MIXER_READ_VOLUME
+  SOUND_MIXER_READ_BASS
+  SOUND_MIXER_READ_TREBLE
+  SOUND_MIXER_READ_SYNTH
+  SOUND_MIXER_READ_PCM
+  SOUND_MIXER_READ_SPEAKER
+  SOUND_MIXER_READ_LINE
+  SOUND_MIXER_READ_MIC
+  SOUND_MIXER_READ_CD
+  SOUND_MIXER_READ_IMIX
+  SOUND_MIXER_READ_ALTPCM
+  SOUND_MIXER_READ_RECLEV
+  SOUND_MIXER_READ_IGAIN
+  SOUND_MIXER_READ_OGAIN
+  SOUND_MIXER_READ_LINE1
+  SOUND_MIXER_READ_LINE2
+  SOUND_MIXER_READ_LINE3
+  SOUND_MIXER_READ_RECSRC
+  SOUND_MIXER_READ_DEVMASK
+  SOUND_MIXER_READ_RECMASK
+  SOUND_MIXER_READ_STEREODEVS
+  SOUND_MIXER_READ_CAPS
+
+  SOUND_MIXER_WRITE_VOLUME
+  SOUND_MIXER_WRITE_BASS
+  SOUND_MIXER_WRITE_TREBLE
+  SOUND_MIXER_WRITE_SYNTH
+  SOUND_MIXER_WRITE_PCM
+  SOUND_MIXER_WRITE_SPEAKER
+  SOUND_MIXER_WRITE_LINE
+  SOUND_MIXER_WRITE_MIC
+  SOUND_MIXER_WRITE_CD
+  SOUND_MIXER_WRITE_IMIX
+  SOUND_MIXER_WRITE_ALTPCM
+  SOUND_MIXER_WRITE_RECLEV
+  SOUND_MIXER_WRITE_IGAIN
+  SOUND_MIXER_WRITE_OGAIN
+  SOUND_MIXER_WRITE_LINE1
+  SOUND_MIXER_WRITE_LINE2
+  SOUND_MIXER_WRITE_LINE3
+  SOUND_MIXER_WRITE_RECSRC
+  SOUND_MIXER_WRITE_DEVMASK
+  SOUND_MIXER_WRITE_RECMASK
+
+  SOUND_MIXER_INFO
+  SOUND_MIXER_AGC
+  SOUND_MIXER_3DSE
+  SOUND_MIXER_PRIVATE1
+  SOUND_MIXER_PRIVATE2
+  SOUND_MIXER_PRIVATE3
+  SOUND_MIXER_PRIVATE4
+  SOUND_MIXER_PRIVATE5
+  SOUND_MIXER_GETLEVELS
+  SOUND_MIXER_SETLEVELS
+
+  OSS_GETVERSION
+
   AFMT_QUERY
   AFMT_MU_LAW
   AFMT_A_LAW
@@ -356,8 +490,6 @@ F<E<lt>sys/soundcard.hE<gt>> or F<E<lt>linux/soundcard.hE<gt>>.
   APF_CPUINTENS
 
 =head1 BUGS
-
-As mentioned above, the mixer interface is not supported.
 
 The C<Makefile.PL> is pretty slow, and could be optimized to check
 more than one constant at once, or all of them at once, even.
